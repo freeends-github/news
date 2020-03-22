b3142385c89761505bb6b1f30bd53e30a8e099c5
Njomeza Nasufi <njnasufi@gmail.com>
2020-03-22 19:29:51 +0100

    updated

---
+++ style.js
@@ -0,0 +1,12 @@
+// Navbar toggler
+$(".toggler").on("click", function() {
+    $(this).toggleClass("open");
+    $("body").toggleClass("navbar-open");
+});
+$(".nav-links a").on("click", function() {
+    if($("body").hasClass("navbar-open")) {
+      $(".toggler").removeClass("open");
+      $("body").removeClass("navbar-open");
+    }
+});
+
